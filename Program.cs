94be00d882f1cb23c13f167fc63a6ab8066ede4d
gebrina <gsonofmika@gmail.com>
2024-03-23 21:19:06 +0300

    configure db context and migrate

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Net_Identity_Auth.DbContext;
+using Net_Identity_Auth.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var DbCon = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw
+new ArgumentException("Invalid connection string");
 // add services
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseNpgsql(DbCon);
+});
+
+builder.Services.AddIdentity<ApplicatoinUser, IdentityRole>()
+.AddEntityFrameworkStores<AuthDbContext>()
+.AddDefaultTokenProviders();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
